a6b9e1e96285d43ceb47a82e5cf61d11e581965b
Ashley Schauer <aelschauer@gmail.com>
2017-03-13 16:22:43 -0600

    update routes for relationship endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :customers, only: [:index, :show] do
+        resources :invoice, only: :index
+        resources :transactions, only: :index
+      end
+      resources :invoices, only: [:index, :show] do
+        resources :customer, only: :index
+        resources :invoice_items, only: :index
+        resources :items, only: :index
+        resources :merchant, only: :index
+        resources :transactions, only: :index
+      end
+      resources :invoice_items, only: [:index, :show] do
+        resources :invoice, only: :index
+        resources :item, only: :index
+      end
+      resources :items, only: [:index, :show] do
+        resources :invoice_items, only: :index
+        resources :merchant, only: :index
+      end
+      resources :merchants, only: [:index, :show] do
+        resources :invoices, only: :index
+        resources :items, only: :index
+      end
+      resources :transactions, only: [:index, :show] do
+        resources :invoice, only: :index
+      end
+    end
+  end
+
 end
