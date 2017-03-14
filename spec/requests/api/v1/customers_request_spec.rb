38ca88e8daec3abff49fd88b357658a447274634
Ashley Schauer <aelschauer@gmail.com>
2017-03-14 14:58:04 -0600

    Add test for find method

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -4,7 +4,7 @@ describe "Customers API" do
   it "sends a list of customers" do
      create_list(:customer, 3)
 
-      get '/api/v1/customers'
+      get "/api/v1/customers"
 
       expect(response).to be_success
 
@@ -22,5 +22,96 @@ describe "Customers API" do
 
     expect(response).to be_success
     expect(customer["id"]).to eq(id)
+  end
+
+  it "can find a customer by its id" do
+    customer1 = create(:customer)
+    customer2 = create(:customer)
+
+    get "/api/v1/customers/find?id=#{customer1.id}"
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(result["id"]).to eq(customer1.id)
+    expect(result["id"]).to_not eq(customer2.id)
+  end
+
+  it "can find a customer by its first name" do
+    customer1 = create(:customer, first_name: "Han")
+    customer2 = create(:customer, first_name: "Han")
+
+    get "/api/v1/customers/find?first_name=Han"
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(result["id"]).to eq(customer1.id)
+    expect(result["id"]).to_not eq(customer2.id)
+  end
+
+  it "can find a customer by its last name" do
+    customer1 = create(:customer, last_name: "Solo")
+    customer2 = create(:customer, last_name: "Solo")
+
+    get "/api/v1/customers/find?last_name=Solo"
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(result["id"]).to eq(customer1.id)
+    expect(result["id"]).to_not eq(customer2.id)
+  end
+
+  it "can find a customer by when it was created" do
+    datetime = "2017-01-01T00:00:00.000Z"
+    customer1 = Customer.create(
+      first_name: "Ashley",
+      last_name: "Schauer",
+      created_at: datetime,
+      updated_at: datetime
+    )
+    customer2 = Customer.create(
+      first_name: "Alex",
+      last_name: "Fosco",
+      created_at: datetime,
+      updated_at: datetime
+    )
+
+    get "/api/v1/customers/find?created_at=#{datetime}"
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(result["id"]).to eq(customer1.id)
+    expect(result["id"]).to_not eq(customer2.id)
+  end
+
+  it "can find a customer by when it was updated" do
+    datetime = "2017-01-01T00:00:00.000Z"
+    customer1 = Customer.create(
+      first_name: "Ashley",
+      last_name: "Schauer",
+      created_at: datetime,
+      updated_at: datetime
+    )
+    customer2 = Customer.create(
+      first_name: "Alex",
+      last_name: "Fosco",
+      created_at: datetime,
+      updated_at: datetime
+    )
+
+    get "/api/v1/customers/find?updated_at=#{datetime}"
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(result["id"]).to eq(customer1.id)
+    expect(result["id"]).to_not eq(customer2.id)
+  end
+
+  it "can find all customers by id" do
+    
   end
 end
