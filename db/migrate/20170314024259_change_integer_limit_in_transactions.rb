970d035148115e29b251c2e43ca7882eacef4912
Ashley Schauer <aelschauer@gmail.com>
2017-03-13 20:59:28 -0600

    add csv files and upload with seed

---
+++ db/migrate/20170314024259_change_integer_limit_in_transactions.rb
@@ -0,0 +1,5 @@
+class ChangeIntegerLimitInTransactions < ActiveRecord::Migration[5.0]
+  def change
+    change_column :transactions, :credit_card_number, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313214149) do
+ActiveRecord::Schema.define(version: 20170314024259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 20170313214149) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.integer  "credit_card_number"
+    t.string   "credit_card_number"
     t.date     "credit_card_expiration_date"
     t.string   "result"
     t.integer  "invoice_id"
