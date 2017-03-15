275f86de240aef8125b3f963808d3494d698ba44
Ashley Schauer <aelschauer@gmail.com>
2017-03-14 18:30:33 -0600

    remove relationships on transaction and invoice serializers

--- app/serializers/invoice_serializer.rb
+++ app/serializers/invoice_serializer.rb
@@ -1,6 +1,3 @@
 class InvoiceSerializer < ActiveModel::Serializer
-  attributes :id, :customer_id, :merchant_id
-
-  belongs_to :customer
-  belongs_to :merchant
+  attributes :id, :customer_id, :merchant_id, :status
 end
