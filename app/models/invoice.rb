fe724e4324d09a3d0d0ebcd8eb9484924d8b3c09
Ashley Schauer <aelschauer@gmail.com>
2017-03-15 21:52:26 -0600

    add random method to models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,8 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  def self.random
+    order("RANDOM()").first
+  end
 end
