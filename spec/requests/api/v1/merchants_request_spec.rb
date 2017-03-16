1716e77dd7e4f9c5938b6ddc15491e89430f8230
Alex Fosco <alex.hf88@gmail.com>
2017-03-15 22:45:57 -0600

    adds relationship endpoints for merchants

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -160,13 +160,47 @@ describe "Merchants API" do
       create_list(:merchant, 3)
 
       get '/api/v1/merchants/random'
-      
+
       merchant = JSON.parse(response.body)
 
       expect(response).to be_success
       expect(merchant).to be_a(Hash)
       expect(merchant).to have_key("id")
       expect(merchant).to have_key("name")
+    end
+  end
+
+  context "relationship methods" do
+    it "can find all the items for a merchant" do
+      merchant = create(:merchant)
+      items = create_list(:item, 2, merchant: merchant)
+      create_list(:item, 3)
+
+      get "/api/v1/merchants/#{merchant.id}/items"
+
+      results = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(results.count).to eq(2)
+
+      expect(results.first["id"]).to eq(items.first.id)
+      expect(results.second["id"]).to eq(items.second.id)
+    end
+
+    it "can find all the invoices for a merchant" do
+      merchant = create(:merchant)
+      invoices = create_list(:invoice, 2, merchant: merchant)
+      create_list(:invoice, 3)
+
+      get "/api/v1/merchants/#{merchant.id}/invoices"
+
+      results = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(results.count).to eq(2)
+
+      expect(results.first["id"]).to eq(invoices.first.id)
+      expect(results.second["id"]).to eq(invoices.second.id)
     end
   end
 
