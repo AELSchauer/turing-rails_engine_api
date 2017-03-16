5a72900cd0081c87d77a7639d3468a8e1423f9e9
Ashley Schauer <aelschauer@gmail.com>
2017-03-16 14:31:07 -0600

    add ActiveRecord query to model with SQL query commented out

--- app/controllers/api/v1/items/best_day_controller.rb
+++ app/controllers/api/v1/items/best_day_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::Items::BestDayController < ApplicationController
   def show
-    render json: Invoice.best_day_for_item(params[:item_id])
+    results = Item.best_day(params[:item_id])
+    best_day = results["created_at"].strftime("%FT%T.%LZ")
+    render json: {"best_day" => best_day}
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,28 @@ class Item < ApplicationRecord
   def self.random
     order("RANDOM()").first
   end
+
+  def self.best_day(id)
+    Invoice.select(:created_at)
+      .joins(invoice_items: :item)
+      .where(items: {id: id})
+      .group(:created_at)
+      .order("sum(invoice_items.quantity) DESC, invoices.created_at DESC")
+      .limit(1)
+      .first
+
+    # query = "SELECT invoices.created_at
+    #   FROM items
+    #   INNER JOIN invoice_items
+    #           ON invoice_items.item_id = items.id
+    #   INNER JOIN invoices
+    #           ON invoices.id = invoice_items.invoice_id
+    #   WHERE items.id = #{id}
+    #   GROUP BY invoices.created_at
+    #   ORDER BY sum(invoice_items.quantity) DESC, invoices.created_at DESC
+    #   LIMIT 1
+    #   ;"
+
+    # results = ActiveRecord::Base.connection.execute(query).first
+  end
 end
