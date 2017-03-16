e3ef6da1d34581cbae285a132583bd36c40f46f8
Ashley Schauer <aelschauer@gmail.com>
2017-03-16 17:06:30 -0600

    update route and controller
    I changed the controller action for merchants/:id/revenue to show instead of index. Index makes more sense for all merchants while show is for a single merchant.

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def index
+
+  end
+  def show
     render json: Merchant.revenue_by_id(params[:merchant_id].to_i)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -52,9 +52,10 @@ Rails.application.routes.draw do
         get "/find", to: "find#show"
         get "/find_all", to: "find#index"
         get "/random", to: "random#show"
+        get "/revenue", to: "revenue#index"
       end
       resources :merchants, only: [:index, :show] do
-        get "/revenue", to: "merchants/revenue#index"
+        get "/revenue", to: "merchants/revenue#show"
         get "/invoices", to: "merchants/invoices#index"
         get "/items", to: "merchants/items#index"
       end
