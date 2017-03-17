98d4b799e02693004838112defbe40a2b1524857
Ashley Schauer <aelschauer@gmail.com>
2017-03-16 18:43:08 -0600

    create uniform strong param naming on all find controllers

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::Items::FindController < ApplicationController
 
   def index
-    render json: Item.where(item_params)
+    render json: Item.where(find_params)
   end
 
 	def show
-		render json: Item.find_by(item_params)
+		render json: Item.find_by(find_params)
 	end
 
 	private
 
-  	def item_params
+  	def find_params
   		params.permit(:id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at)
   	end
 
