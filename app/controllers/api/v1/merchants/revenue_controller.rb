64725a7ff04af258903c7f11ad699ac435739296
Ashley Schauer <aelschauer@gmail.com>
2017-03-16 18:09:57 -0600

    add date parameter to query method on controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def index
-    render json: Merchant.total_revenue
+    render json: Merchant.total_revenue(params[:date])
   end
   def show
     render json: Merchant.revenue_by_id(params[:merchant_id].to_i)
